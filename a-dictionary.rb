7e5d5925a8b6b10e16d4a87de71077dd43048188
Adam Woodcock <adam.woodcock98@gmail.com>
2022-02-08 10:05:06 +0000

    Added a small example of a dictionary using hashes

---
+++ a-dictionary.rb
@@ -0,0 +1,13 @@
+# bear: a creature that fishes in the river for salmon.
+# river: a body of water that contains salmon, and sometimes bears.
+# salmon: a fish, sometimes in a river, sometimes in a bear, and sometimes in both.
+
+animals = {
+  :bear => "a creature that fishes in the river for salmon.",
+  :river => "a body of water that contains salmon, and sometimes bears.",
+  :salmon => "a fish, sometimes in a river, sometimes in a bear, and sometimes in both."
+}
+
+puts animals[:bear]
+puts animals[:river]
+puts animals[:salmon]
